04b9baf697be6d0b9486456c6f335142dcf0d1b6
rp-psiphon <r.peccia@psiphon.ca>
2022-08-14 15:47:07 -0400

    added HelloChrome102 and HelloFirefox102

--- u_common.go
+++ u_common.go
@@ -152,6 +152,7 @@ var (
 	HelloFirefox_63   = ClientHelloID{helloFirefox, "63", nil}
 	HelloFirefox_65   = ClientHelloID{helloFirefox, "65", nil}
 	HelloFirefox_99   = ClientHelloID{helloFirefox, "99", nil}
+	HelloFirefox_102  = ClientHelloID{helloFirefox, "102", nil}
 
 	HelloChrome_Auto = HelloChrome_100
 	HelloChrome_58   = ClientHelloID{helloChrome, "58", nil}
@@ -161,7 +162,8 @@ var (
 	HelloChrome_83   = ClientHelloID{helloChrome, "83", nil}
 	HelloChrome_87   = ClientHelloID{helloChrome, "87", nil}
 	HelloChrome_96   = ClientHelloID{helloChrome, "96", nil}
-	HelloChrome_100  = ClientHelloID{helloFirefox, "100", nil}
+	HelloChrome_100  = ClientHelloID{helloChrome, "100", nil}
+	HelloChrome_102  = ClientHelloID{helloChrome, "102", nil}
 
 	HelloIOS_Auto = HelloIOS_12_1
 	HelloIOS_11_1 = ClientHelloID{helloIOS, "111", nil} // legacy "111" means 11.1
